73b80a705b2144d0d527ca5886012c2b625f6261
Dearning <1835985714@qq.com>
2022-02-05 15:49:59 +0800

    modified github pages path to test multipages

---
+++ .history/src/resources/textures_20220205114449.js
@@ -0,0 +1,63 @@
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = '../src/jsm/TODO.png';
+// 第二块展板图片地址
+billboardTextures.musicTexture =
+  '../src/jsm/TODO.png';
+// 第三块展板图片地址
+billboardTextures.fundTexture =
+  '../src/jsm/TODO.png';
+
+let boxTexture = {};
+// 链接的图标地址
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.BiliBili = '../src/jsm/BiliBili.png';
+boxTexture.QQ = '../src/jsm/qq.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+boxTexture.edmText = '../src/jsm/EDM.png';
+
+// 砖块材质
+let stoneTexture = '../src/jsm/stone.png';
+// 展板木头材质
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+// 文字图片地址
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/TODO.png';
+inputText.activities = '../src/jsm/TODO.png';
+inputText.bagholderBetsText = '../src/jsm/TODO.png';
+inputText.homeSweetHomeText = '../src/jsm/TODO.png';
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+inputText.pcControl = '../src/jsm/pc-control.png'
+inputText.mobileControl = '../src/jsm/mobile-control.png'
+inputText.link = '../src/jsm/link.png'
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+// 链接的跳转地址
+let URL = {};
+URL.blog =
+  'https://dearning.cn';
+URL.ryanfloyd = 'https://dearning.cn';
+URL.fund = 'https://github.com/Dearning/3DEntrance';
+URL.gitHub = 'https://github.com/Dearning';
+URL.BiliBili = 'https://space.bilibili.com/568066331';
+URL.email = 'https://dylansjc@outlook.com';
+URL.music = 'https://music.huabyte.com';
+URL.devTo =
+  'https://qm.qq.com/cgi-bin/qm/qr?k=R9NJf-CX4BjpWQMSzhDtKnqsP_NrcbpR&noverify=0';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
